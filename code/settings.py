58493602aa81d55155fef6880613d00609a7d64f
Pawn Zhou(周明洁) <pawn.zhou@QUECTEL.COM>
2022-05-16 16:38:02 +0800

    update code/settings

--- code/settings.py
+++ code/settings.py
@@ -12,62 +12,155 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import uos
+import ure
+import ql_fs
+import ujson
+import modem
+import _thread
 
-import net
-
-# Quecthing 连接参数
-QUECIOT = {
-    # CMCC 移动
-    "CMCC": {
-        "PK": "p111SP",
-        "PS": "bHBoM01JL1htTEsw",
-        "LIFETIME": 65500,
-        "MODE": 0, # 0 LWM2M, 1 MQTT
-        "SERVER": "http://iot-south.quectel.com:5683"
-    },
-    # CT 电信
-    "CT": {
-        "PK": "p1126D",
-        "PS": "WFJ5a3FDQXBIM2tJ",
-        "LIFETIME": 65500,
-        "MODE": 0, # 0 LWM2M, 1 MQTT
-        "SERVER": "coap://221.229.214.202:5683"
-    }
-}
-
-# Quecthing 物模型事件数据
-# 物模型功能ID
-SWATCH_STATUS = 1   # 门磁开关状态
-SOC = 2             # 电量
-SIGNAL = 3          # 信号强度
-OPEN_ALARM = 6      # 门磁告警
-HEARTBEAT = 7       # 心跳
-LOW_POWER_ALARM = 8 # 低电量告警
-MANUAL_ALARM = 9    # 手动告警
-ALARM_OR_RECOVER = 10 # 低电量告警状态，正常告警or恢复
-
-# 业务心跳
-HEARTBEAT_DICT = {HEARTBEAT: {SOC: None, SIGNAL: None}}
-# 门磁告警
-OPEN_ALARM_DICT = {OPEN_ALARM: {SWATCH_STATUS: None, SOC: None, SIGNAL: None}}
-# 低电量告警
-LOW_POWER_ALARM_DICT = {LOW_POWER_ALARM: {SOC: None, SIGNAL: None, ALARM_OR_RECOVER: True}}
-# 手动告警
-MANUAL_ALARM_DICT = {MANUAL_ALARM: {SOC: None, SIGNAL: None}}
+from usr.modules.common import Singleton
+from usr.modules.common import option_lock
+from usr.settings_sys import SYSConfig
+from usr.settings_queccloud import QuecCloudConfig
 
 
-class ConfigModule(object):
+PROJECT_NAME = "QuecPython-GateMonitor"
 
-    @staticmethod
-    def quecIotConnectParam():
-        '''
-        根据运营商返回quecIot连接属性
-        '''
-        operator = net.operatorName()[1]
-        if operator == "CT":
-            return QUECIOT.get("CT")
-        else:
-            return QUECIOT.get("CMCC")
+PROJECT_VERSION = "1.0.0"
+
+DEVICE_FIRMWARE_NAME = uos.uname()[0].split("=")[1]
+
+DEVICE_FIRMWARE_VERSION = modem.getDevFwVersion()
+
+_settings_lock = _thread.allocate_lock()
 
 
+class Settings(Singleton):
 
+    def __init__(self, settings_file="/usr/gateMonitor_settings.json"):
+        self.settings_file = settings_file
+        self.current_settings = {}
+        self.init()
+
+    def __init_config(self):
+        try:
+            self.current_settings["sys"] = {k: v for k, v in SYSConfig.__dict__.items() if not k.startswith("_")}
+
+            # CloudConfig init
+            if self.current_settings["sys"]["cloud"] == SYSConfig._cloud.quecIot:
+                self.current_settings["cloud"] = {k: v for k, v in QuecCloudConfig.__dict__.items() if not k.startswith("_")}
+
+            # LocConfig init
+
+            # UserConfig init
+            return True
+        except:
+            return False
+
+    def __read_config(self):
+        if ql_fs.path_exists(self.settings_file):
+            with open(self.settings_file, "r") as f:
+                self.current_settings = ujson.load(f)
+                return True
+        return False
+
+    def __set_config(self, opt, val):
+        if opt in self.current_settings["user_cfg"]:
+            if opt == "phone_num":
+                if not isinstance(val, str):
+                    return False
+                pattern = ure.compile(r"^(?:(?:\+)86)?1[3-9]\d\d\d\d\d\d\d\d\d$")
+                if pattern.search(val):
+                    self.current_settings["user_cfg"][opt] = val
+                    return True
+                return False
+            elif opt in ("work_cycle_period", "over_speed_threshold"):
+                if not isinstance(val, int):
+                    return False
+                if val < 1:
+                    return False
+                self.current_settings["user_cfg"][opt] = val
+                return True
+            elif opt in ("low_power_alert_threshold", "low_power_shutdown_threshold"):
+                if not isinstance(val, int):
+                    return False
+                if val < 0 or val > 100:
+                    return False
+                self.current_settings["user_cfg"][opt] = val
+                return True
+            elif opt in ("sw_ota", "sw_ota_auto_upgrade", "sw_voice_listen", "sw_voice_record",
+                         "sw_fault_alert", "sw_low_power_alert", "sw_over_speed_alert",
+                         "sw_sim_abnormal_alert", "sw_disassemble_alert", "sw_drive_behavior_alert"):
+                if not isinstance(val, bool):
+                    return False
+                self.current_settings["user_cfg"][opt] = val
+                return True
+            elif opt == "ota_status":
+                if not isinstance(val, dict):
+                    return False
+                self.current_settings["user_cfg"][opt] = val
+                return True
+            elif opt in ("user_ota_action", "drive_behavior_code"):
+                if not isinstance(val, int):
+                    return False
+                self.current_settings["sys"][opt] = val
+                return True
+        elif opt == "cloud":
+            if not isinstance(val, dict):
+                return False
+            self.current_settings[opt] = val
+            return True
+
+        return False
+
+    def __save_config(self):
+        try:
+            with open(self.settings_file, "w") as f:
+                ujson.dump(self.current_settings, f)
+            return True
+        except:
+            return False
+
+    def __remove_config(self):
+        try:
+            uos.remove(self.settings_file)
+            return True
+        except:
+            return False
+
+    def __get_config(self):
+        return self.current_settings
+
+    @option_lock(_settings_lock)
+    def init(self):
+        if self.__read_config() is False:
+            if self.__init_config():
+                return self.__save_config()
+        return False
+
+    @option_lock(_settings_lock)
+    def get(self):
+        return self.__get_config()
+
+    @option_lock(_settings_lock)
+    def set(self, opt, val):
+        return self.__set_config(opt, val)
+
+    @option_lock(_settings_lock)
+    def save(self):
+        return self.__save_config()
+
+    @option_lock(_settings_lock)
+    def remove(self):
+        return self.__remove_config()
+
+    @option_lock(_settings_lock)
+    def reset(self):
+        if self.__remove_config():
+            if self.__init_config():
+                return self.__save_config()
+        return False
+
+
+settings = Settings()
